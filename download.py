f82bd14e773018b6b7d020268ec355bbeacf1c64
hamzeasadi <hamzeasadi1@gmail.com>
2023-01-04 20:42:54 +0000

    download edit

--- download.py
+++ download.py
@@ -2,52 +2,13 @@ import requests
 import os
 import conf as cfg
 
-# def download_file_from_google_drive(id, destination):
-#     # URL = "https://docs.google.com/uc?export=download"
-#     URL = "https://drive.google.com/file/d/1c84_CtRGK8ifv-7c9MI_xYVruYAnYX5p/view?usp=share_link"
-
-#     session = requests.Session()
-
-#     response = session.get(URL, params = { 'id' : id , 'confirm': 1 }, stream = True)
-#     token = get_confirm_token(response)
-
-#     if token:
-#         params = { 'id' : id, 'confirm' : token }
-#         response = session.get(URL, params = params, stream = True)
-
-#     save_response_content(response, destination)    
-
-# def get_confirm_token(response):
-#     for key, value in response.cookies.items():
-#         if key.startswith('download_warning'):
-#             return value
-
-#     return None
-
-# def save_response_content(response, destination):
-#     CHUNK_SIZE = 32768
-
-#     with open(destination, "wb") as f:
-#         for chunk in response.iter_content(CHUNK_SIZE):
-#             if chunk: # filter out keep-alive new chunks
-#                 f.write(chunk)
-
-# if __name__ == "__main__":
-#     file_id = '1c84_CtRGK8ifv-7c9MI_xYVruYAnYX5p'
-#     destination = cfg.paths['data']
-#     download_file_from_google_drive(file_id, destination)
-
-
-
-
-
 def download_file_from_google_drive(id, destination):
-    URL = "https://docs.google.com/uc?export=download"
-    # URL = "https://drive.google.com/file/d/1c84_CtRGK8ifv-7c9MI_xYVruYAnYX5p/view?usp=share_link"
+    # URL = "https://docs.google.com/uc?export=download"
+    URL = "https://drive.google.com/file/d/1c84_CtRGK8ifv-7c9MI_xYVruYAnYX5p/view?usp=share_link"
 
     session = requests.Session()
 
-    response = session.get(URL, params = { 'id' : id }, stream = True)
+    response = session.get(URL, params = { 'id' : id , 'confirm': 1 }, stream = True)
     token = get_confirm_token(response)
 
     if token:
@@ -75,3 +36,42 @@ if __name__ == "__main__":
     file_id = '1c84_CtRGK8ifv-7c9MI_xYVruYAnYX5p'
     destination = cfg.paths['data']
     download_file_from_google_drive(file_id, destination)
+
+
+
+
+
+# def download_file_from_google_drive(id, destination):
+#     URL = "https://docs.google.com/uc?export=download"
+#     # URL = "https://drive.google.com/file/d/1c84_CtRGK8ifv-7c9MI_xYVruYAnYX5p/view?usp=share_link"
+
+#     session = requests.Session()
+
+#     response = session.get(URL, params = { 'id' : id }, stream = True)
+#     token = get_confirm_token(response)
+
+#     if token:
+#         params = { 'id' : id, 'confirm' : token }
+#         response = session.get(URL, params = params, stream = True)
+
+#     save_response_content(response, destination)    
+
+# def get_confirm_token(response):
+#     for key, value in response.cookies.items():
+#         if key.startswith('download_warning'):
+#             return value
+
+#     return None
+
+# def save_response_content(response, destination):
+#     CHUNK_SIZE = 32768
+
+#     with open(destination, "wb") as f:
+#         for chunk in response.iter_content(CHUNK_SIZE):
+#             if chunk: # filter out keep-alive new chunks
+#                 f.write(chunk)
+
+# if __name__ == "__main__":
+#     file_id = '1c84_CtRGK8ifv-7c9MI_xYVruYAnYX5p'
+#     destination = cfg.paths['data']
+#     download_file_from_google_drive(file_id, destination)
