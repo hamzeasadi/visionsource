401f6a9f6863abd05eb7be5459b2c8346807c5cd
hamzeasadi <hamzeasadi1@gmail.com>
2023-01-07 09:16:55 +0000

    old test

--- train.py
+++ train.py
@@ -58,7 +58,7 @@ def main():
 
     # if True:
     if args.test:
-        model_name = f"vision_residual_1.pt"
+        # model_name = f"vision_residual_1.pt"
         state = keeptrack.load_ckp(fname=model_name)
         Net.load_state_dict(state['model'], strict=False)
         print(f"min error is {state['minerror']} which happen at epoch {state['epoch']}")
